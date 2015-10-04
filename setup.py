f1935725d00e6182ac246d022cbce1437ad0d3b3
kgullikson88 <kevin.gullikson@gmail.com>
2015-10-04 09:55:47 -0500

    Added setup script.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+
+setup(name='fitting_utilities',
+      version='0.1.0',
+      description='Various useful classes for fitting stuff.',
+      author='Kevin Gullikson',
+      author_email='kevin.gullikson@gmail.com',
+      packages=['fitters'])
