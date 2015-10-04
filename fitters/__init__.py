aa5d3d9bdfb80508d391e553e068d9a7f5e65dec
kgullikson88 <kevin.gullikson@gmail.com>
2015-10-04 11:01:14 -0500

    Added classifiers to setup.py

--- fitters/__init__.py
+++ fitters/__init__.py
@@ -1,2 +1,4 @@
-from bayesian import Bayesian_LS
-from linear import RobustFit, ChebFit2D
+from __future__ import print_function, division, absolute_import
+
+from .bayesian import Bayesian_LS
+from .linear import RobustFit, ChebFit2D
--- setup.py
+++ setup.py
@@ -6,4 +6,12 @@ setup(name='fitting_utilities',
       description='Various useful classes for fitting stuff.',
       author='Kevin Gullikson',
       author_email='kevin.gullikson@gmail.com',
+      license='BSD',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: BSD License',
+          'Programming Language :: Python',
+          'Topic :: Scientific/Engineering :: Astronomy',
+      ],
       packages=['fitters'])
